d9004aacefeaf20b46a1a9f70699a3a54adbf1c3
Abraham Russell <flyingcycling@outlook.com>
2019-11-19 23:27:36 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, message} from "antd";
+import {Link, BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import loadable from "@loadable/component";
+
+const Loading = () => <div>...</div>;
+
+const Home = loadable(() => import("./Home"), {
+  fallback: <Loading />
+});
+
+const About = loadable(() => import("./About"), {
+  fallback: <Loading />
+});
+
+const Topics = loadable(() => import("./Topics"), {
+  fallback: <Loading />
+});
 
 function App() {
+  useEffect(() => {
+    message.success('my love');
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,9 +36,36 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button>Learn React</Button>
         </a>
       </header>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/topics">
+              <Topics />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
